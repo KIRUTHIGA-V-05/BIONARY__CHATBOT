f1529a953b8c8cca92efbda4317ca6ddc0769b0a
KIRUTHIGA-V-05 <kiruthiga3000@gmail.com>
2025-12-06 20:08:08 +0530

    Refactor add_new_event function for clarity

--- frontend.py
+++ frontend.py
@@ -3,7 +3,6 @@ import psycopg2
 from pgvector.psycopg2 import register_vector
 from sentence_transformers import SentenceTransformer
 import numpy as np
-import uuid
 
 model = SentenceTransformer("BAAI/bge-base-en-v1.5", trust_remote_code=True)
 
@@ -32,7 +31,6 @@ def add_new_event(form_data):
     conn = _connect_to_db()
     if not conn:
         return False, "Database connection error."
-
     try:
         register_vector(conn)
         event_id = form_data.get("name_of_event")
@@ -40,7 +38,6 @@ def add_new_event(form_data):
         embedding_vector = model.encode(search_text)
         if isinstance(embedding_vector, np.ndarray):
             embedding_vector = embedding_vector.tolist()
-
         parms = (
             event_id,
             0,
@@ -59,7 +56,6 @@ def add_new_event(form_data):
             search_text,
             embedding_vector,
         )
-
         with conn.cursor() as cur:
             cur.execute(
                 """
@@ -77,6 +73,5 @@ def add_new_event(form_data):
             conn.commit()
         conn.close()
         return True, "Event added."
-
     except Exception as e:
         return False, str(e)
